cff5ea9c13fe7c1d1d415c05b1f377df38403bbe
ito ryo <electrooculogram@gmail.com>
2025-03-26 23:55:28 +0900

    add mysql container

--- admin_docroot/index.php
+++ admin_docroot/index.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+try {
+    $dsn = 'mysql:host=mysql;port=3306;dbname=dev;charset=utf8mb4';
+    $username = 'user';
+    $password = 'password';
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    $pdo = new PDO($dsn, $username, $password, $options);
+    echo "MySQL connected successfully.";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
